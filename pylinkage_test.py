540c852811540b5203a32dc1a0905b1dbd9290c1
limch998-kyo <61343063+limch998-kyo@users.noreply.github.com>
2023-10-16 16:28:12 +0900

    added function linkage code

--- pylinkage_test.py
+++ pylinkage_test.py
@@ -1,15 +1,19 @@
 import pylinkage as pl
 
 # Main motor
-crank = pl.Crank(0, 1, joint0=(0, 0), angle=.31, distance=1)
+crank = pl.Crank(0, 1, joint0=(0, 0), angle=.31, distance=1, name="B")
 # Close the loop
 pin = pl.Pivot(
     3, 2, joint0=crank, joint1=(3, 0), 
-    distance0=3, distance1=1
+    distance0=3, distance1=1, name="C"
 )
 
-# Create the linkage
-my_linkage = pl.Linkage(joints=(crank, pin))
+# Linkage definition
+my_linkage = pl.Linkage(
+    joints=(crank, pin),
+    order=(crank, pin),
+    name="My four-bar linkage"
+)
 
-# Show the results
+# Visualization
 pl.show_linkage(my_linkage)
